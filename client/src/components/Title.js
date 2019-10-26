cc24878932a552e3e389da6b41cf7da82e23e866
Francesco Commisso <francescocommissodev@gmail.com>
2019-10-26 19:01:50 -0400

    test

--- client/src/components/Title.js
+++ client/src/components/Title.js
@@ -3,7 +3,7 @@ import React from "react";
 function Title() {
   return (
     <div>
-      <h1>Transactions</h1>
+      <h1>Transactions Yo</h1>
       <p>Understand what the fuck your're spending your money on</p>
     </div>
   );
