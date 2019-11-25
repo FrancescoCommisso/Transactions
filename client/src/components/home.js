4136a19a405671035b673c96c352d1cb5b62a216
Francesco Commisso <francesco@Francescos-MBP.home>
2019-11-25 18:22:48 -0500

    saving

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -7,7 +7,9 @@ import {
   H3,
   P,
   colors,
-  WidgetComponent
+  WidgetComponent,
+  WidgetContent,
+  Widget
 } from "./common";
 import styled from "styled-components";
 import ring from "../assets/ring.svg";
@@ -33,23 +35,35 @@ export const Home = () => {
           <P>Scroll to learn how it works</P>
         </div>
       </CenterPage>
-      <CenterPage style={{ flexWrap: "wrap" }}>
-        <CenterDiv>
-          <WidgetComponent title="Add your sources of income"></WidgetComponent>
+      <CenterPage style={{ flexWrap: "wrap", height: "100vh" }}>
+        <CenterDiv style={{ marginTop: "5%" }}>
+          <WidgetComponent
+            title="Add your sources of income"
+            content={
+              <div style={{ display: "flex", flexWrap: "wrap" }}>
+                <Widget style={{ minHeight: "0px" }}>
+                  <ul>
+                    <li>Add all the different ways that you make money</li>
+                  </ul>
+                </Widget>
+                <Widget style={{ minHeight: "0px" }}>graphic</Widget>
+              </div>
+            }
+          ></WidgetComponent>
         </CenterDiv>
       </CenterPage>
       <CenterPage>
-        <CenterDiv>
+        <CenterDiv style={{ marginTop: "5%" }}>
           <WidgetComponent title="Create your budgets"></WidgetComponent>
         </CenterDiv>
       </CenterPage>
       <CenterPage>
-        <CenterDiv>
+        <CenterDiv style={{ marginTop: "5%" }}>
           <WidgetComponent title="Create a budget period"></WidgetComponent>
         </CenterDiv>
       </CenterPage>
       <CenterPage>
-        <CenterDiv>
+        <CenterDiv style={{ marginTop: "5%" }}>
           <WidgetComponent title="Add transactions once every budget period"></WidgetComponent>
         </CenterDiv>
       </CenterPage>
