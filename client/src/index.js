73ac31331133c2c1b698f648402c411052dbbdda
Francesco Commisso <francesco@Francescos-MacBook-Pro.local>
2019-12-02 20:43:31 -0500

    hardcoded urls

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,8 @@ import "semantic-ui-css/semantic.min.css";
 import { Auth0Provider } from "./react-auth0-spa";
 import config from "./auth_config.json";
 import WebFont from "webfontloader";
-const { REDIRECT_URL = "http://localhost:3000/callback" } = process.env;
+impor;
+const { REDIRECT_URL = "http://paychunk.herokuapp.com/callback" } = process.env;
 console.log("REDIRECT URL: ", REDIRECT_URL);
 
 WebFont.load({
